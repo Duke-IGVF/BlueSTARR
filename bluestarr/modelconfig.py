f0f3c741146c2e6ae335b21ce8507ae68255cb6d
Hilmar Lapp <hlapp@drycafe.net>
2024-08-09 23:38:14 -0400

    Makes ModelConfig inherit from configparser and adds getlist()

--- bluestarr/modelconfig.py
+++ bluestarr/modelconfig.py
@@ -1,18 +1,22 @@
 import configparser
 import sys
 
-class ModelConfig:
+class ModelConfig(configparser.ConfigParser):
     def __init__(self,
-                 config_file,defaults={"MaxTrain": sys.maxsize, "MaxTest": sys.maxsize}, **kwargs):
-        config = configparser.ConfigParser(
-            defaults= defaults,
-            converters= {'any': _auto_cast},
-            **kwargs)
+                 config_file,
+                 defaults={"MaxTrain": sys.maxsize,
+                           "MaxTest": sys.maxsize,
+                           "Flatten": '0',
+                           "ShouldTest": '0'},
+                 **kwargs):
+        super().__init__(defaults= defaults,
+                         converters= {'any': _auto_cast, 'list': _aslist},
+                         **kwargs)
         with open(config_file, 'r') as f:
             cfgContent = f.read()
         cfgContent = "[NN]\n" + cfgContent
-        config.read_string(cfgContent)
-        self.config = config['NN']
+        self.read_string(cfgContent)
+        self.config = self['NN']
     
     def __getattr__(self, name: str):
         val = self.config.get(name)
@@ -34,3 +38,9 @@ def _auto_cast(value):
     except ValueError:
         pass
     return value
+
+def _aslist(value):
+    if value is None:
+        return [value]
+    vals = value.split(',')
+    return [ _auto_cast(x) for x in vals ]
