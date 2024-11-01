4a45bbdf127230574ff0070e559d859696308ef7
Hilmar Lapp <hlapp@drycafe.net>
2024-11-01 19:38:22 -0400

    Adds scripts to fix TensorRT library failing to be found

---
+++ tensorrt-fix.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Fix for TensorRT libraries not being found, resulting in 
+# Tensorflow not being able to load the TensorRT plugin.
+#
+# This script will create a symbolic link to the TensorRT libraries
+# with the correct version number in the name.
+
+# Check if the TensorRT libraries are present
+if python3 -c "import tensorrt_libs" &> /dev/null; then
+    tensorrt_lib=$(python -c "import os; import tensorrt_libs; print(os.path.dirname(tensorrt_libs.__file__))")
+    echo "TensorRT library found at: $tensorrt_lib"
+
+    # Check if the full version library is present
+    libnvinfer=$(ls ${tensorrt_lib}/libnvinfer.so.* 2> /dev/null)
+    libbuilder=$(ls ${tensorrt_lib}/libnvinfer_builder_resource.so.* 2> /dev/null)
+    if [[ -z "$libbuilder" || -z "$libnvinfer" ]]; then
+        echo "Expected library or libraries not found, skipping fix."
+        exit 1
+    fi
+    vinfer=${libnvinfer##*.so.}
+    vbuilderdiff=${libbuilder##*.so.${vinfer}}
+    if [[ -z $vbuilderdiff ]]; then
+        echo "Builder library version is the same as the main library, skipping fix."
+    else
+        for lib in $tensorrt_lib/lib*.so.${vinfer} ; do
+            echo "Creating symbolic link with version ${vinfer}${vbuilderdiff} for "$(basename $lib)
+            ln -s $lib $tensorrt_lib/$(basename $lib)${vbuilderdiff}
+        done
+    fi
+else
+    echo "TensorRT library not found, skipping fix."
+    exit 0
+fi
